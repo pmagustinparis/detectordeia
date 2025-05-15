5538753369d161f767ecbd6adc097d031e348017
Agustin Paris <parisagustin@gmail.com>
2025-05-15 16:20:09 -0300

    feat: agregar favicon.ico y referencia en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicon.png" sizes="16x16" />
       </head>
